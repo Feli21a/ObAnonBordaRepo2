f490deda868ae2d07c96e40e6f18d039823d91d1
Feli21a <felianon14@gmail.com>
2024-11-04 12:27:51 -0300

    inicializacion

---
+++ PreguntIA/src/main/java/ObligatorioDDA_IS/PreguntIaApplication.java
@@ -0,0 +1,13 @@
+package ObligatorioDDA_IS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreguntIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreguntIaApplication.class, args);
+	}
+
+}
---
+++ PreguntIA/src/test/java/ObligatorioDDA_IS/PreguntIaApplicationTests.java
@@ -0,0 +1,13 @@
+package ObligatorioDDA_IS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreguntIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
