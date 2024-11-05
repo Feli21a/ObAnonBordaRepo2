cb0483ef52c85d2e7683576c41c035c4a6d905d9
feli21a <felianon14@gmail.com>
2024-11-05 12:34:31 -0300

    metodo logout, y login con sistema de sesion activa

--- PreguntIA/src/main/java/ObligatorioDDA_IS/Controllers/UserController.java
+++ PreguntIA/src/main/java/ObligatorioDDA_IS/Controllers/UserController.java
@@ -8,36 +8,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ObligatorioDDA_IS.DTO.UserRegistrationDTO;
+import ObligatorioDDA_IS.Models.User;
 import ObligatorioDDA_IS.Services.UserService;
-
-// Controlador que expone los endpoints de registro y autenticación de usuarios.
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
     @Autowired
     private UserService usuarioService;
 
-    // Usando @ModelAttribute para recibir el formulario completo
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@ModelAttribute UserRegistrationDTO user) {
         try {
             usuarioService.registerUser(user);
-            ResponseEntity.ok("Registro exitoso");
-            return ResponseEntity.ok().body("/login.html");
+            return ResponseEntity.ok("Registro exitoso");
         } catch (RuntimeException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> authenticateUser(@ModelAttribute UserRegistrationDTO user) {
+    public ResponseEntity<String> authenticateUser(@ModelAttribute UserRegistrationDTO user, HttpSession session) {
         try {
-            usuarioService.authenticateUser(user.getEmail(), user.getPassword());
-            return ResponseEntity.ok().body("/menu.html");
-
+            User loggedInUser = usuarioService.authenticateUser(user.getEmail(), user.getPassword());
+            session.setAttribute("user", loggedInUser); // Agrega el usuario a la sesión
+            return ResponseEntity.ok().body("/Menu.html"); // Redirige a la página de menú
         } catch (RuntimeException e) {
             return ResponseEntity.status(401).body(e.getMessage());
         }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpSession session) {
+        session.invalidate(); // Invalida la sesión actual
+        return ResponseEntity.ok("Sesión cerrada con éxito");
     }
 }
